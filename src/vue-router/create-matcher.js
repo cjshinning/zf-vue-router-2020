319873051935d87f67f519ae49d8ae541cfce54a
cjshinningg <784319417@qq.com>
2024-03-14 11:18:16 +0800

    5.实现动态格式化用户路由参数

--- src/vue-router/create-matcher.js
+++ src/vue-router/create-matcher.js
@@ -1,14 +1,17 @@
-import { createRouteMap } from './create-route-map'
+import createRouteMap from './create-route-map'
 
 export default function createMatcher(routes) {
   // pathList 会吧所有的路由 组成一个数组 ['/','/about','/about/a','/about/b']
   // pathList {'/':{},'/about':{},'/about/a':{}}
   let { pathList, pathMap } = createRouteMap(routes);
+  addRoutes([{ path: '/xxx', component: {} }])
+  console.log(pathList, pathMap);
   function match() { //等会要提供用户输入的路径 获取对应的匹配记录
 
   }
-  function addRoutes() {
-
+  // 具备新增和添加的功能
+  function addRoutes(routes) {  //routes动态添加的路由
+    createRouteMap(routes, pathList, pathMap);
   }
   return {
     match,
